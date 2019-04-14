e394c7dec024108a5af6999f90bd164ecb31dd68
TiagoOlivv <tiago.-oliveira@hotmail.com>
2019-04-13 21:56:54 -0300

    ip e porta dinamicamente

--- middleware.py
+++ middleware.py
@@ -1,13 +1,18 @@
-import requests, string, asyncio, time
+import requests, string, asyncio, time, socket
 from flask import Flask, jsonify
 from threading import Thread
 
 app = Flask(__name__)
+hostname = socket.gethostname()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('localhost', 0))
+port = sock.getsockname()[1]
+sock.close()
 
 @app.route('/mid/<name>', methods = ['GET','POST'])
 def main(name):
 
-    caixa =  requests.post("http://localhost:5001/%s" % (name))
+    caixa =  requests.post("http://192.168.0.7:5001/%s" % (name))
     
     return jsonify({
     	"Entrada": caixa.json()['enviado'],
@@ -15,4 +20,4 @@ def main(name):
     })
 
 if __name__ == '__main__':
-    app.run(threaded = True, debug = True, host= 'localhost',  port = 4000)
+    app.run(threaded = True, debug = True, host = socket.gethostbyname(hostname),  port = (port))
--- servidorA.py
+++ servidorA.py
@@ -1,7 +1,14 @@
+import string, socket
 from flask import Flask, jsonify
-import string
+
 
 app = Flask(__name__)
+hostname = socket.gethostname()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('localhost', 0))
+port = sock.getsockname()[1]
+sock.close()
+
 
 @app.route('/<name>', methods=['GET','POST'])
 def main(name):
@@ -14,4 +21,4 @@ def main(name):
 	return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug = True,host = 'localhost', port = 5001)
+    app.run(debug = True, host = socket.gethostbyname(hostname), port = (port))
